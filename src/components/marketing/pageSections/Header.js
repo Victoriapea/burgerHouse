b31ac74106462e993096d60c482142ebda1cce4e
Victoria Pea <victoriapea@outlook.fr>
2024-04-08 13:54:40 +0200

    last products

--- src/components/marketing/pageSections/Header.js
+++ src/components/marketing/pageSections/Header.js
@@ -5,7 +5,7 @@ import Button from "../../elements/Button";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between py-10">
+    <div className="relative z-10 flex items-center justify-between py-10">
       <div className=" h-20 w-full">
         <img src={Logo} alt="Grill Spot House" className="w-64" />
       </div>
@@ -13,10 +13,15 @@ export default function Header() {
       <div className="w-full text-secondary">
         <div className="flex items-center justify-end">
           <img src={Icone} alt="" className="w-5 h-5 mr-1" />
-          <span>Commandez votre repas en ligne</span>
+          <span className="font-semibold">Commandez votre repas en ligne</span>
         </div>
-        <div>
-          <Button />
+        <div className="mt-5 flex items-center justify-end ">
+          <Button className="tracking-widest mr-3">
+            Inscription
+          </Button>
+          <Button className="tracking-widest" color="secondary">
+            Connexion
+          </Button>
         </div>
 
 
--- src/components/marketing/pageSections/HeroTop.js
+++ src/components/marketing/pageSections/HeroTop.js
@@ -1,16 +1,52 @@
-import React from 'react'
-import Illustration from '../../../medias/images/background/bg-food.jpg'
-import Container from '../../elements/Container'
-import Header from './Header'
+import React from "react";
+import Illustration from "../../../medias/images/background/bg-food.jpg";
+import Container from "../../elements/Container";
+import Header from "./Header";
+import Product from "../../../medias/images/heroTop/burger-hero-top.png";
+import Button from "../../elements/Button";
 
 export default function HeroTop() {
   return (
-    <div className='bg-primary w-full h-screen'>
-      <div className='bg-repeat w-full h-full' style={{ backgroundImage: `url(${Illustration})` }}>
+    <div className="bg-primary w-full">
+      <div
+        className="bg-repeat w-full h-full pb-20"
+        style={{ backgroundImage: `url(${Illustration})` }}
+      >
         <Container>
           <Header />
+          <div className="relative mt-28 w-full">
+            <img
+              src={Product}
+              className="absolute z-0 -top-64 right-0 w-6/12"
+              alt="Un menu hamburger avec frites et coca cola."
+            />
+            <div className="absolute right-80 top-40 rounded-full bg-danger w-40 h-40 p-3">
+              <div className="w-full h-full rounded-full border-2 border-dashed border-white text-white flex items-center justify-center">
+                <div className="text-center">
+                  <span className="block font-extrabold">
+                    <span className="text-5xl">5</span>.49€
+                  </span>
+                  <span className="tracking-widest uppercase text-sm">seulement</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="relative z-10 text-secondary uppercase">
+              <h1 className="mb-4 font-semibold">
+                C'est le moment de goûter au bon goût des hamburgers.
+              </h1>
+              <h2 className="font-secondary shadowTitleSecondary">
+                <span className="text-8xl block">Burger</span>
+                <span className="text-6xl">House</span>
+                <span className="text-4xl ml-3">Click<span className="text-danger">&</span>collect</span>
+              </h2>
+            </div>
+          </div>
+          <Button className="mt-5 rounded-md" color="secondary" theme="big">
+            Créer mon compte
+          </Button>
         </Container>
       </div>
     </div>
-  )
+  );
 }
