7dd5218645610c42f9e9086ca8b28deb69a4914c
Victoria Pea <victoriapea@outlook.fr>
2024-04-08 16:24:48 +0200

    products

---
+++ src/components/marketing/pageSections/Products.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import Container from '../../elements/Container'
+import HeadingTitles from '../../elements/DisplayTitles/HeadingTitles'
+import Heading from '../../elements/DisplayTitles/Heading'
+import Button from '../../elements/Button'
+import Product1 from '../../../medias/images/products/Product-1.jpg';
+import Product2 from '../../../medias/images/products/Product-2.jpg';
+import Product3 from '../../../medias/images/products/Product-3.jpg';
+
+export default function Products() {
+  return (
+    <Container>
+      <HeadingTitles>
+        Toujours des délicieux burgers
+      </HeadingTitles>
+
+      <Heading theme="secondary">
+        Choisissez et savourez
+      </Heading>
+      <p className='text-center'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+
+      <div className='grid grid-cols-3 gap-x-1 mb-20 mt-10'>
+
+        <div className='w-full h-full'>
+          <div className='relative w-full h-72'>
+            <img src={Product1} alt="Un hamburger" className='bg-primary absolute top-0 left-0 w-full h-full object-cover z-0' />
+          </div>
+
+          <div className='flex flex-col items-center justify-center px-5 pb-5'>
+            <Heading variant="h3">
+              Lorem ipsum dolor
+            </Heading>
+            <p className='text-center'>
+              Lorem ipsum dolor is simply text for designer and developer.
+            </p>
+            <Button color="danger" className="font-secondary">
+              Commandez
+            </Button>
+          </div>
+        </div>
+
+        <div className='w-full h-full'>
+          <div className='relative w-full h-72'>
+            <img src={Product2} alt="Un hamburger" className='bg-primary absolute top-0 left-0 w-full h-full object-cover z-0' />
+          </div>
+
+          <div className='flex flex-col items-center justify-center px-5 pb-5'>
+            <Heading variant="h3">
+              Lorem ipsum dolor
+            </Heading>
+            <p className='text-center'>
+              Lorem ipsum dolor is simply text for designer and developer.
+            </p>
+            <Button color="danger" className="font-secondary">
+              Commandez
+            </Button>
+          </div>
+        </div>
+
+        <div className='w-full h-full'>
+          <div className='relative w-full h-72'>
+            <img src={Product3} alt="Un hamburger" className='bg-primary absolute top-0 left-0 w-full h-full object-cover z-0' />
+          </div>
+
+          <div className='flex flex-col items-center justify-center px-5 pb-5'>
+            <Heading variant="h3">
+              Lorem ipsum dolor
+            </Heading>
+            <p className='text-center'>
+              Lorem ipsum dolor is simply text for designer and developer.
+            </p>
+            <Button color="danger" className="font-secondary">
+              Commandez
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroTop from "../components/marketing/pageSections/HeroTop";
 import LastProducts from "../components/marketing/pageSections/LastProducts";
+import Products from "../components/marketing/pageSections/Products";
 
 export default function HomePage() {
   return (
     <>
       <HeroTop />
       <LastProducts />
+      <Products />
     </>
   )
 }
